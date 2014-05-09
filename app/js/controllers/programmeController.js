e2b09419259ff5748e5de0eae23b002a4bc18283
LAU Thierry <thierlau@gmail.com>
2014-05-09 02:06:51 +0200

    more programs

--- app/js/controllers/programmeController.js
+++ app/js/controllers/programmeController.js
@@ -7,7 +7,7 @@ angular.module('ThierryEtClaire').controller('programmeController', function ($s
 		location : {
 			name: "Mairie de Bezannes",
 			address1: "1 rue Source de Muire",
-			address2: "51726 Bezannes Cédex",
+			address2: "51726 Bezannes",
 			map : {
 				center : {
 					latitude: 49.2201195,
@@ -38,6 +38,44 @@ angular.module('ThierryEtClaire').controller('programmeController', function ($s
 		when : {
 			date: "Samedi 13 Juin 2015",
 			time: "12H30"
+		}
+	}, {
+		name: "Vin d'Honneur",
+		text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem esse et, laborum libero numquam odit omnis pariatur totam veritatis voluptatum. Aliquid cumque eligendi est et inventore laboriosam, numquam odit veniam?",
+		location : {
+			name: "Salle Hélios à la Mairie de Bezannes",
+			address1: "1 rue Source de Muire",
+			address2: "51726 Bezannes",
+			map : {
+				center : {
+					latitude: 49.2201195,
+					longitude: 3.9859497
+				},
+				zoom: 16
+			}
+		},
+		when : {
+			date: "Samedi 13 Juin 2015",
+			time: "13H30"
+		}
+	}, {
+		name: "Diner de mariage",
+		text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem esse et, laborum libero numquam odit omnis pariatur totam veritatis voluptatum. Aliquid cumque eligendi est et inventore laboriosam, numquam odit veniam?",
+		location : {
+			name: "Vilmart & Cie",
+			address1: "5 rue des Gravières",
+			address2: "51500 Rilly la Montagne",
+			map : {
+				center : {
+					latitude: 0,
+					longitude: 0
+				},
+				zoom: 16
+			}
+		},
+		when : {
+			date: "Samedi 13 Juin 2015",
+			time: "20H00"
 		}
 	}];
 });
