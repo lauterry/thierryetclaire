1c7e5f653b87184368a3d91fc8257ec0c05613ce
LAU Thierry <thierlau@gmail.com>
2014-05-10 01:10:08 +0200

    add gallery

---
+++ app/js/controllers/galleryController.js
@@ -0,0 +1,10 @@
+angular.module('ThierryEtClaire').controller('galleryController', function ($scope) {
+    "use strict";
+
+	$scope.photos = [
+		{ url : "img/photo1.jpg" },
+		{ url : "img/photo2.jpg" },
+		{ url : "img/photo3.jpg" }
+	];
+
+});
