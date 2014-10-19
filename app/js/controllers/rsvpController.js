8069c148e4ea024d3242609a8130a03d42118d31
LAU Thierry <thierlau@gmail.com>
2014-10-19 23:03:43 +0200

    add validation to form and migrate to angular 1.3.0

--- app/js/controllers/rsvpController.js
+++ app/js/controllers/rsvpController.js
@@ -1,6 +1,11 @@
 angular.module('ThierryEtClaire').controller('rsvpController', function ($scope) {
     "use strict";
 
+	$scope.contact = {
+		nombreAdultes : 1,
+		nombreEnfants : 0
+	};
+
 	$scope.sendResponse = function () {
 
 	};
