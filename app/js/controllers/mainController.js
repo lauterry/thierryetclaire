4b0a890654258315a6648a1d1e8b4ca60c89d74c
LAU Thierry <thierlau@gmail.com>
2014-10-19 21:18:06 +0200

    zsh:1: command not found: wq

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,7 +1,4 @@
-angular.module('ThierryEtClaire').controller('mainController', function ($scope, parallaxHelper) {
+angular.module('ThierryEtClaire').controller('mainController', function () {
     "use strict";
 
-	$scope.fast = parallaxHelper.createAnimator(0.6, 400, -100, 500);
-
-	$scope.slow = parallaxHelper.createAnimator(0.3, 300, -100, 300);
 });
