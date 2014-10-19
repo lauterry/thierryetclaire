4078ddb87a119108d35450b48c5741f8d32a59f3
LAU Thierry <thierlau@gmail.com>
2014-10-19 21:37:40 +0200

    add dotline directives

---
+++ app/js/directives/dotline.js
@@ -0,0 +1,9 @@
+angular.module('ThierryEtClaire').directive('dotline', function () {
+	"use strict";
+
+	return {
+		restrict: 'E',
+		templateUrl: 'js/directives/dotline.html',
+		replace : true
+	};
+});
