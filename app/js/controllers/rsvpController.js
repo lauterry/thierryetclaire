275e9dd2fe4847503c474403d2b0f446d405c87e
LAU Thierry <thierlau@gmail.com>
2014-11-03 01:30:38 +0100

    add validation to form

--- app/js/controllers/rsvpController.js
+++ app/js/controllers/rsvpController.js
@@ -2,11 +2,14 @@ angular.module('ThierryEtClaire').controller('rsvpController', function ($scope)
     "use strict";
 
 	$scope.contact = {
-		nombreAdultes : 1,
-		nombreEnfants : 0
+		adults : 1,
+		children : 0
 	};
 
 	$scope.sendResponse = function () {
+		if ($scope.rsvpForm.$valid) {
+			console.log(JSON.stringify($scope.contact));
+		}
 
 	};
 
