3a2bde0c8f2a65b3b5f2adf1600f2cab3c1c496f
LAU Thierry <thierlau@gmail.com>
2014-11-18 00:24:39 +0100

    add scroll reveal effects

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,22 @@
+/* jshint -W117*/
+/* jshint -W055*/
+
 angular.module('ThierryEtClaire', ['google-maps']);
 
 angular.module('ThierryEtClaire').config(function ($httpProvider) {
 	"use strict";
 
 	$httpProvider.defaults.headers.post['Content-Type'] = 'application/xml;charset=utf-8';
+});
+
+
+angular.module('ThierryEtClaire').run(function () {
+	"use strict";
+
+	window.sr = new scrollReveal({
+		reset : true,
+		delay : 'always',
+		mobile : false
+	});
 });
 
